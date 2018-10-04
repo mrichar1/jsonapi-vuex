474616c038f4ff853893a3956f43c59617ebd01d
Matthew Richardson <m.richardson@ed.ac.uk>
2018-10-04 21:35:32 +0100

    Add test entries.

--- spec/jsonapi-vuex.spec.js
+++ spec/jsonapi-vuex.spec.js
@@ -12,13 +12,45 @@ describe("jsonapi-vuex tests", function() {
     it("should export actions", function() {
       expect(_testing.actions).to.be.an('object');
     });
+    describe("fetch", function() {
+        it("should make an api call to GET item(s)")
+        it("should add record(s) to the store")
+        it("should fail gracefully")
+    })
 
+    describe("create", function() {
+      it("should make an api call to POST item(s)")
+      it("should add record(s) to the store")
+      it("should fail gracefully")
+    })
+
+    describe("update", function() {
+      it("should make an api call to PATCH item(s)")
+      it("should update record(s) in the store")
+      it("should fail gracefully")
+
+    })
+
+    describe("delete", function() {
+      it("should make an api call to DELETE item(s)")
+      it("should delete record(s) from the store")
+      it("should fail gracefully")
+    })
   });
 
   describe("jsonapiModule mutations", function() {
     it("should export mutations", function() {
       expect(_testing.mutations).to.be.an('object');
     });
+
+    describe("add_record", function() {
+      it("should add records to the store")
+
+      it("should add a single item to the store")
+
+      it("should fail gravefully")
+
+    })
 
     describe("delete_record", function() {
       it("should delete a single record", function() {
@@ -29,9 +61,12 @@ describe("jsonapi-vuex tests", function() {
             }
           }
         }
+
         _testing.mutations.delete_record(state, {type: 'widgets', id: '1'})
         expect(state['records']['widgets']).to.not.have.key('1');
       });
+
+      it("should gracefully handle delete requests for non-existent objects")
     })
   });
 
