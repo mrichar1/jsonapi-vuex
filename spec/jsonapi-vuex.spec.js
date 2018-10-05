75bfc59329ed15362d3cba32260c4f460bf9805a
Matthew Richardson <m.richardson@ed.ac.uk>
2018-10-05 10:18:11 +0100

    Share common data variables, defined using beforeEach.

--- spec/jsonapi-vuex.spec.js
+++ spec/jsonapi-vuex.spec.js
@@ -1,6 +1,51 @@
 import 'chai/register-expect';
 import { jsonapiModule, _testing } from '../src/jsonapi-vuex.js';
 
+// 'global' variables (redefined in beforeEach)
+
+var state, item1, item2, norm_item1, norm_item2, record, norm_record
+
+beforeEach(function() {
+// Set up commonly used data objects
+
+  state = {records: {}}
+
+  item1 = {
+    id: '1',
+    type: 'widget',
+    attributes: {'foo': 1}
+    }
+
+  item2 = {
+    id: '2',
+    type: 'widget',
+    attributes: {'foo': 2}
+  }
+
+  norm_item1 = {
+    'widget': {
+      '1': {
+        attributes: {'foo': 1}
+      }
+    }
+  }
+
+  norm_item2 = {
+    'widget': {
+      '2': {
+        attributes: {'foo': 2}
+      }
+    }
+  }
+
+  record = [
+    item1, item2
+  ]
+
+  norm_record = {widget: {...norm_item1['widget'], ...norm_item2['widget']}}
+
+})
+
 describe("jsonapi-vuex tests", function() {
 
   it("should export jsonapiModule", function() {
@@ -48,15 +93,6 @@ describe("jsonapi-vuex tests", function() {
     describe("addRecord", function() {
       it("should add a record to Vue store", function() {
         const { addRecord } = _testing
-        const state = {records: {}}
-        const record = [
-          {
-            'id': '1',
-            'type': 'widget',
-            'attributes': {'foo': '1'}
-          }
-        ]
-
         addRecord(state, record)
         expect(state['records']).to.have.key('widget')
       });
@@ -64,20 +100,7 @@ describe("jsonapi-vuex tests", function() {
     describe("normalizeItem", function() {
       it("should normalize a single item", function() {
         const { normalizeItem } = _testing
-        const item = {
-          id: '1',
-          type: 'widget',
-          attributes: {'foo': 1}
-        }
-
-        const denorm_item = {
-          'widget': {
-            '1': {
-              attributes: {'foo': 1}
-            }
-          }
-        }
-        expect(normalizeItem(item)).to.deep.equal(denorm_item)
+        expect(normalizeItem(item1)).to.deep.equal(norm_item1)
       });
     })
 
@@ -85,101 +108,25 @@ describe("jsonapi-vuex tests", function() {
     describe("normalize", function() {
       it("should normalize a single item", function() {
         const { normalize } = _testing
-        const item = {
-          id: '1',
-          type: 'widget',
-          attributes: {'foo': 1}
-        }
-
-        const denorm_item = {
-          'widget': {
-            '1': {
-              attributes: {'foo': 1}
-            }
-          }
-        }
-        expect(normalize(item)).to.deep.equal(denorm_item)
+        expect(normalize(item1)).to.deep.equal(norm_item1)
       });
 
       it("should normalize an array of records", function() {
         const { normalize } = _testing
-        const record = [
-          {
-            id: '1',
-            type: 'widget',
-            attributes: {'foo': 1}
-          },
-          {
-            id: '2',
-            type: 'widget',
-            attributes: {'foo': 2}
-          }
-        ]
-
-        const denorm_record = {
-          'widget': {
-            '1': {
-              attributes: {'foo': 1}
-            },
-            '2': {
-              attributes: {'foo': 2}
-            }
-          }
-        }
-        expect(normalize(record)).to.deep.equal(denorm_record)
+        expect(normalize(record)).to.deep.equal(norm_record)
       });
     }); // normalize
 
     describe("denormalize", function() {
       it("should denormalize multiple items", function() {
-         const { denormalize } = _testing
-
-         const denorm_record = {
-           'widget': {
-            '1': {
-               attributes: {'foo': 1}
-             },
-             '2': {
-               attributes: {'foo': 2}
-             }
-           }
-         }
-
-         const record = [
-           {
-             id: '1',
-             type: 'widget',
-             attributes: {'foo': 1}
-           },
-           {
-             id: '2',
-             type: 'widget',
-             attributes: {'foo': 2}
-           }
-         ]
-
-        expect(denormalize(denorm_record)).to.deep.equal(record)
+        const { denormalize } = _testing
+        expect(denormalize(norm_record)).to.deep.equal(record)
 
       });
 
       it("should denormalize a single item", function() {
-         const { denormalize } = _testing
-
-         const denorm_record = {
-           'widget': {
-            '1': {
-               attributes: {'foo': 1}
-             }
-           }
-         }
-
-         const record = {
-             id: '1',
-             type: 'widget',
-             attributes: {'foo': 1}
-         }
-
-         expect(denormalize(denorm_record)).to.deep.equal(record)
+        const { denormalize } = _testing
+        expect(denormalize(norm_item1)).to.deep.equal(item1)
       });
 
     }); // denormalize
